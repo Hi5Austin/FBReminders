70981368434bfa103a9bcca598a49bbdbb998039
austincarvey <austin@carvey.me>
2015-01-22 22:18:50 -0500

    textEnter.rb now goes to google.com and puts 'I am not a human being' into the search bar

--- textEnter.rb
+++ textEnter.rb
@@ -1,3 +1,7 @@
 require 'selenium-webdriver'
-driver = Selenium::WebDriver.for :chrome
-driver.navigate.to 'https://www.google.com/'
+
+driver = Selenium::WebDriver.for :chrome # instantiates a google chrome session
+driver.navigate.to 'https://www.google.com/' # takes you to google.com
+
+googleBar = driver.find_element(:id,"gbqfq") # finds the google search bar
+googleBar.send_keys "I am not a human being" # puts "I am not a human being" in the search bar
