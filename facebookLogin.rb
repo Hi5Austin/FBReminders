61630e4bc1caeb45662d22251fe308e3841fecee
austincarvey <austin@carvey.me>
2015-01-24 21:27:20 -0500

    facebookLogin.rb now clicks on the first user and sends them the message 'i am not a human being'

--- facebookLogin.rb
+++ facebookLogin.rb
@@ -8,22 +8,19 @@ passwordBar = driver.find_element(:id,"pass") #find password input bar
 loginButton = driver.find_element(:id,"u_0_n") #finds login button
 
 emailBar.send_keys "austinspreadsheet@gmail.com" # puts in the email
-passwordBar.send_keys "YOURPASSWORD" # puts in the password
+passwordBar.send_keys "iamfakesiri" # puts in the password
 loginButton.click # clicks the login button
 
 driver.manage.window.resize_to(1300,940) #maximizes the window
 
-friends = driver.find_elements(:class,"_55lr") # gather all your friends
+driver.manage.timeouts.implicit_wait = 7 #waits for friends to load in
 
-puts friends
+users = driver.find_elements(:class,"_55ln") #finds the user's picture in the chat w/ friends bar
+users[0].click  #clicks the user's picture
 
-#links = driver.find_elements(:class,"fbNubButton") # finds the chat bar
-#driver.manage.timeouts.page_load = 10
-#links[0].click # opens the chat bar
-#links[1].click # NOTE that sometime it works and sometimes it doesn't but if you click both it usually works, so the error is ok
+uiOptions = driver.find_elements(:class, "_552m") #Finds all user input bars
 
-#driver.execute_script("$('._55lr').click()",element)
+chat = uiOptions[0] #Selects the chat bar
 
-#friends = driver.find_elements(:class,"_55lr") # gather all your friends
-
-#puts friends
+chat.send_keys "I am not a human being" #Puts in a message
+chat.send_keys :return # Sends the message
